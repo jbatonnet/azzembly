209ad6de76279cef198c17793827ec154c9d8b09
Julien Batonnet <julien.batonnet@marinepress.com>
2018-06-15 08:41:06 -0400

    Sourcetree fixes

---
+++ SourceTree/SimpleTextDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Azzembly.Patcher;
+
+namespace SourceTree.View.Dialogs
+{
+    [Patch(typeof(SimpleTextDialog))]
+    public class SimpleTextDialogPatch : SimpleTextDialog
+    {
+        private new void TextField_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.TextField.SelectAll();
+        }
+    }
+}
